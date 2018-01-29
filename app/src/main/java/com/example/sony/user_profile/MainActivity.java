f836476c8570c0d98e48b8a1f2f2c34fb1d94677
bhanukunduru <bhanuchandra449@gmail.com>
2018-01-29 05:30:48 -0800

    Firebase Login And Register Credentials

--- app/src/main/java/com/example/sony/user_profile/MainActivity.java
+++ app/src/main/java/com/example/sony/user_profile/MainActivity.java
@@ -4,11 +4,15 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -38,11 +42,14 @@ public class MainActivity extends AppCompatActivity {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if (user != null) {
                     // User is signed in
+                    Toast.makeText(getApplicationContext(),"Loggedin As :"+user.getEmail(),
+                            Toast.LENGTH_LONG).show();
 
                 } else {
                     // User is signed out
                     Toast.makeText(getApplicationContext(),"User is Not Loggedin",
                             Toast.LENGTH_LONG).show();
+
                 }
             }
         };
@@ -50,11 +57,52 @@ public class MainActivity extends AppCompatActivity {
 
     public void login(View v){
 
-
-
+        mAuth.signInWithEmailAndPassword(et1.getText().toString(), et4.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
-
     public void register(View v){
 
+        mAuth.createUserWithEmailAndPassword(et1.getText().toString(),
+                et4.getText().toString())
+
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Authentication Success.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+        mAuth.signOut();
+    }
+
 }
