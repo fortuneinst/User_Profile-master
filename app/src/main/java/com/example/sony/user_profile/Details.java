9777e924b64ef9e0857c107a8c688643dfa5b90a
bhanukunduru <bhanuchandra449@gmail.com>
2018-01-31 02:51:13 -0800

    New Class

---
+++ app/src/main/java/com/example/sony/user_profile/Details.java
@@ -0,0 +1,37 @@
+package com.example.sony.user_profile;
+
+/**
+ * Created by SONY on 31-01-2018.
+ */
+
+public class Details {
+
+    private String username;
+    private Integer id;
+    private Integer mobilenumber;
+    private String email;
+
+    public Details(){}
+    public Details(String username, Integer id, Integer mobilenumber, String email) {
+
+    }
+
+    public String getusername() {
+        return getusername();
+    }
+
+    public Integer getSid() {
+        return getSid();
+    }
+
+    public Integer getMobilenumber() {
+        return getMobilenumber();
+    }
+
+    public boolean getemail() {
+        return getemail();
+    }
+
+
+}
+
