560e1e8888fa3c418aa3e2cee36dbe188a94a933
bhanukunduru <bhanuchandra449@gmail.com>
2018-02-03 08:38:26 -0800

    New names

--- app/src/main/java/com/example/sony/user_profile/Details.java
+++ app/src/main/java/com/example/sony/user_profile/Details.java
@@ -6,41 +6,42 @@ package com.example.sony.user_profile;
 
 public class Details {
 
-    private String username;
-    private int id;
-    private int mobilenumber;
-    private String email;
+    private String userName;
+    private String id;
+    private String phoneNumber;
+    private String emailId;
 
-    public Details(){
-
+    public String getUserName() {
+        return userName;
     }
 
-    public void setusername(String username) {
-        this.username = username;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
-    public void setId (int id) {
-        this.id = id;
-    }
-    public void setMobilenumber(int mobilenumber) {
-        this.mobilenumber = mobilenumber;
-    }
-    public void setEmail (String email) {
-        this.email = email;
-    }
-    public String getUsername() {
-        return username;
-    }
-    public int getId() {
+
+    public String  getId() {
         return id;
     }
-    public int getMobilenumber() {
-        return mobilenumber;
-    }
-    public String getemail() {
-        return email;
+
+    public void setId(String id) {
+        this.id = id;
     }
 
+    public String  getPhoneNumber() {
+        return phoneNumber;
+    }
 
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmailId() {
+        return emailId;
+    }
+
+    public void setEmailId(String emailId) {
+        this.emailId = emailId;
+    }
 
 
 }
