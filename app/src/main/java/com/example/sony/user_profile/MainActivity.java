64d6e6c049865c947d6cfa5aab78fca531587720
bhanukunduru <bhanuchandra449@gmail.com>
2018-02-05 23:26:48 -0800

    Changes2

--- app/src/main/java/com/example/sony/user_profile/MainActivity.java
+++ app/src/main/java/com/example/sony/user_profile/MainActivity.java
@@ -74,23 +74,7 @@ public class MainActivity extends AppCompatActivity {
                         }
                     }
                 });
-       /* mAuth.signInWithCustomToken(mCustomToken)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        if (task.isSuccessful()) {
-                            // Sign in success, update UI with the signed-in user's information
-                            Log.d(TAG, "signInWithCustomToken:success");
-                            FirebaseUser user = mAuth.getCurrentUser();
-                        } else {
-                            // If sign in fails, display a message to the user.
-                            Log.w(TAG, "signInWithCustomToken:failure", task.getException());
-                            Toast.makeText(MainActivity.this, "Authentication failed.",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    }
 
-                });*/
     }
 
 
